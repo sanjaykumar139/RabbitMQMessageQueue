2a38a4b8217004a06fdd13fecfb110b6c284a984
sanjay k <sanjaykumar139@gmail.com>
2023-03-23 14:17:11 +0530

    rabit mq with .net core

---
+++ RabbitMQProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RabbitMQProductAPI.Model;
+using RabbitMQProductAPI.RabbitMQ;
+using RabbitMQProductAPI.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RabbitMQProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService productService;
+        private readonly IRabitMQProducer _rabitMQProducer;
+        public ProductController(IProductService _productService, IRabitMQProducer rabitMQProducer)
+        {
+            productService = _productService;
+            _rabitMQProducer = rabitMQProducer;
+        }
+        [HttpGet("productlist")]
+        public IEnumerable<Product> ProductList()
+        {
+            var productList = productService.GetProductList();
+            return productList;
+        }
+        [HttpGet("getproductbyid")]
+        public Product GetProductById(int Id)
+        {
+            return productService.GetProductById(Id);
+        }
+        [HttpPost("addproduct")]
+        public Product AddProduct(Product product)
+        {
+            var productData = productService.AddProduct(product);
+            //send the inserted product data to the queue and consumer will listening this data from queue
+            _rabitMQProducer.SendProductMessage(productData);
+            return productData;
+        }
+        [HttpPut("updateproduct")]
+        public Product UpdateProduct(Product product)
+        {
+            return productService.UpdateProduct(product);
+        }
+        [HttpDelete("deleteproduct")]
+        public bool DeleteProduct(int Id)
+        {
+            return productService.DeleteProduct(Id);
+        }
+    }
+}
